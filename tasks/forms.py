9f70a7ad599a9a473c0aad79eed8f9f2f8ca7138
CrisjoSsj <crisjcq2004@gmail.com>
2024-09-29 23:03:22 -0500

    Add files via upload

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,10 +6,9 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ['title','description','important']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control',
-                                            'placeholder':'Write a title'}),
-            'description': forms.Textarea(attrs={'class': 'form-control',
-                                                 'placeholder':'Write a description'}),
-            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
+            'title':forms.TextInput(attrs={'class':'form-control',
+                                           'placeholder':'write a title'}),
+            'description':forms.Textarea(attrs={'class':'form-control',
+                                           'placeholder':'write a description'}),                               
+            'important':forms.CheckboxInput(attrs={'class':'form-check-input m-auto'}),
         }
-
